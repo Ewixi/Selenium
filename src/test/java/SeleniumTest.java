c6a6c5232c1a23aa54f5b3d2159694c131352ad0
Ewixi <tester oprogramowania>
2022-12-25 11:02:48 +0100

    Selenium Tutorial

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,8 +1,14 @@
+import com.google.common.annotations.VisibleForTesting;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class SeleniumTest {
     public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
+    }
+    @Test
+    public void sampleTest() {
+        System.out.println("Hello");
     }
 }
